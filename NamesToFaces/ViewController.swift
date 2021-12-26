d01d01f5b0f31e78580b8ca876aeb5a773e3b60d
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2021-12-26 21:13:10 +0300

    Added imagePickerController & CollectionView Methods.

--- NamesToFaces/ViewController.swift
+++ NamesToFaces/ViewController.swift
@@ -7,13 +7,46 @@
 
 import UIKit
 
-class ViewController: UICollectionViewController {
+class ViewController: UICollectionViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+  //MARK: - ViewController Lifecycle
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewPerson))
   }
 
+  //MARK: UIMethods
+  @objc func addNewPerson() {
+    let picker = UIImagePickerController()
+    picker.allowsEditing = true
+    picker.delegate = self
+    present(picker, animated: true)
+  }
+
+  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+    guard let image = info[.editedImage] as? UIImage else { return }
+    let imageName = UUID().uuidString
+    let imagePath = getDocumetsDirectory().appendingPathComponent(imageName)
+    if let jpegData = image.jpegData(compressionQuality: 0.8){
+      try? jpegData.write(to: imagePath)
+    }
+    dismiss(animated: true)
+  }
+
+  func getDocumetsDirectory() -> URL {
+    let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+    return paths[0]
+  }
+
+  //MARK: CollectionView methods
+  override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 10
+  }
+
+  override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else { fatalError("Unable to dequeue PersonCell!") }
+    return cell
+  }
 
 }
 
