146a3c43b3e35ff03a3f65089ae1255db7bd2f72
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2021-12-26 20:43:31 +0300

    Added collectionView with custom cells

---
+++ NamesToFaces/PersonCell.swift
@@ -0,0 +1,13 @@
+//
+//  PersonCell.swift
+//  NamesToFaces
+//
+//  Created by Николай Никитин on 26.12.2021.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+  @IBOutlet var imageView: UIImageView!
+  @IBOutlet var name: UILabel!
+}
